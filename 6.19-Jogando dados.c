aeaaa9df48edcdb06b4a4f208fa9c4159737ef88
vinicius maia <viniciusmaia140@gmail.com>
2026-01-21 14:26:20 -0400

    Lançando dados e mostrando frequência de somas

--- 6.19-Jogando dados.c
+++ 6.19-Jogando dados.c
@@ -1,5 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define LANCE 36000
 
 int main(void){
+	srand(time(NULL));
+
+	int d1, d2, result, frequencia[13]={0};
+	
+	for(int i = 0; i < LANCE; i++){
+		d1 = 1+rand()%6;
+		d2 = 1+rand()%6;
+		result = d1+d2;
+		frequencia[result]++;
+	}
+
+	printf("Frequencia de resultados:\n ");
+
+	for(int i = 2; i < 13; i++){
+		printf("%d - %d\n", i, frequencia[i]);
+	}
+
+	
+
+
 	return 0;
 }
